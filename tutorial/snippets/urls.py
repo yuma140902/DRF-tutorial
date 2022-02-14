27e93a371a6b30a77a9fa1358cf5f8ca61497ff1
yuma140902 <yuma140902@gmail.com>
2022-02-14 16:47:19 +0900

    update view

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -4,6 +4,8 @@ from snippets import views
 
 urlpatterns = [
   path('snippets/', views.SnippetList.as_view()),
+  path('users/', views.UserList.as_view()),
+  path('users/<int:pk>/', views.UserDetail.as_view()),
   path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
 ]
 
--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,6 +1,7 @@
+from django.contrib.auth.models import User
 from rest_framework import generics
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer, UserSerializer
 
 """
 List all code snippets, or create a new snippet.
@@ -15,3 +16,11 @@ Retrieve, update, or delete a code snippet.
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
   queryset = Snippet.objects.all()
   serializer_class = SnippetSerializer
+
+class UserList(generics.ListAPIView):
+  queryset = User.objects.all()
+  serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+  queryset = User.objects.all()
+  serializer_class = UserSerializer
