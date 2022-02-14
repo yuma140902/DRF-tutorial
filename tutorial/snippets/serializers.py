b7ab21f24000ea79dde4380daf2e55eca6c19c20
yuma140902 <yuma140902@gmail.com>
2022-02-14 16:52:34 +0900

    add readonly owner column

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -3,6 +3,7 @@ from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 class SnippetSerializer(serializers.ModelSerializer):
+  owner = serializers.ReadOnlyField(source='owner.username')
   class Meta:
     model = Snippet
     fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner', 'highlighted']
