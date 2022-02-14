22b964b61eeb24919a5e3730891d4640227af378
yuma140902 <yuma140902@gmail.com>
2022-02-14 16:54:16 +0900

    permission

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import generics
+from rest_framework import permissions
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
 
@@ -9,6 +10,7 @@ List all code snippets, or create a new snippet.
 class SnippetList(generics.ListCreateAPIView):
   queryset = Snippet.objects.all()
   serializer_class = SnippetSerializer
+  permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
   def perform_create(self, serializer):
     serializer.save(owner=self.request.user)
@@ -19,6 +21,7 @@ Retrieve, update, or delete a code snippet.
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
   queryset = Snippet.objects.all()
   serializer_class = SnippetSerializer
+  permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 class UserList(generics.ListAPIView):
   queryset = User.objects.all()
