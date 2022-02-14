f17aa5fd87ca11efff6c78f31141f65749358474
yuma140902 <yuma140902@gmail.com>
2022-02-14 16:30:15 +0900

    more mixin

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,4 +1,3 @@
-from rest_framework import mixins
 from rest_framework import generics
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
@@ -6,32 +5,13 @@ from snippets.serializers import SnippetSerializer
 """
 List all code snippets, or create a new snippet.
 """
-class SnippetList(mixins.ListModelMixin,
-                  mixins.CreateModelMixin,
-                  generics.GenericAPIView):
+class SnippetList(generics.ListCreateAPIView):
   queryset = Snippet.objects.all()
   serializer_class = SnippetSerializer
-  def get(self, request, *args, **kwargs):
-    return self.list(request, *args, **kwargs)
-  
-  def post(self, request, *args, **kwargs):
-    return self.create(request, *args, **kwargs)
 
 """
 Retrieve, update, or delete a code snippet.
 """
-class SnippetDetail(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
   queryset = Snippet.objects.all()
   serializer_class = SnippetSerializer
-
-  def get(self, request, *args, **kwargs):
-    return self.retrieve(request, *args, **kwargs)
-  
-  def put(self, request, *args, **kwargs):
-    return self.update(request, *args, **kwargs)
-
-  def delete(self, request, *args, **kwargs):
-    return self.destroy(self, request, *args, **kwargs)
